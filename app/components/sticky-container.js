b523deea1eb652c0459ccfbc645152930c570358
Brandon Hilkert <brandonhilkert@gmail.com>
2015-06-24 09:10:38 -0400

    Make component name jive with internals

---
+++ app/components/sticky-container.js
@@ -0,0 +1,4 @@
+import Ember from 'ember';
+import StickyContainer from 'ember-cli-sticky/components/sticky-container';
+
+export default StickyContainer;
--- app/components/sticky.js
+++
@@ -1,4 +0,0 @@
-import Ember from 'ember';
-import Sticky from 'ember-cli-sticky/components/sticky';
-
-export default Sticky;
