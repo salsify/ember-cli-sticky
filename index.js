65bffd26c1382a5074a6bd4bba8c7cc88b573122
Dan Freeman <dfreeman@salsify.com>
2016-04-05 10:39:50 -0400

    Pull jquery-sticky from npm instead of Bower

--- index.js
+++ index.js
@@ -4,14 +4,15 @@
 module.exports = {
   name: 'ember-cli-sticky',
 
-  included: function(app) {
-    this._super.included(app);
-
-    if (!process.env.EMBER_CLI_FASTBOOT) {
-      // If this flag is present, the addon is being built in FastBoot
-      // The jQuery plugin causes FastBoot to crash, so only import in
-      // the browser build
-      app.import(app.bowerDirectory + '/sticky/jquery.sticky.js');
+  options: {
+    nodeAssets: {
+      'jquery-sticky': function() {
+        // The jQuery plugin causes FastBoot to crash, so exclude there
+        return {
+          enabled: !process.env.EMBER_CLI_FASTBOOT,
+          import: ['jquery.sticky.js']
+        };
+      }
     }
   }
 };
