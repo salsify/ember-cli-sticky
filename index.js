0950694f7ee8c3c216e733c75dc5a816f35670d0
Dan McClain <git@danmcclain.net>
2016-04-13 08:55:01 -0400

    Only import the jQuery plugin outside of fastboot

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ module.exports = {
   included: function(app) {
     this._super.included(app);
 
-    app.import(app.bowerDirectory + '/sticky/jquery.sticky.js');
+    if (!process.env.EMBER_CLI_FASTBOOT) {
+      // If this flag is present, the addon is being built in FastBoot
+      // The jQuery plugin causes FastBoot to crash, so only import in
+      // the browser build
+      app.import(app.bowerDirectory + '/sticky/jquery.sticky.js');
+    }
   }
 };
