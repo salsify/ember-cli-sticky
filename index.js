725595d605960e1895af8a5df4c35ca88684fd90
Kevin Yang <kyang@salsify.com>
2017-06-30 13:45:30 -0400

    Prepare for FastBoot 1.0 (#20)
    * Prepare for FastBoot 1.0
    
    * Simplify

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 /* jshint node: true */
 'use strict';
 
+const fastbootTransform = require('fastboot-transform');
 module.exports = {
   name: 'ember-cli-sticky',
 
   options: {
     nodeAssets: {
-      'jquery-sticky': function() {
-        // The jQuery plugin causes FastBoot to crash, so exclude there
-        return {
-          enabled: !process.env.EMBER_CLI_FASTBOOT,
-          import: ['jquery.sticky.js']
-        };
+      'jquery-sticky': {
+        import: {
+          include: ['jquery.sticky.js'],
+          // The jQuery plugin causes FastBoot to crash, so use fastboot-transform to exclude it
+          processTree(input) {
+            return fastbootTransform(input);
+          }
+        }
       }
     }
   }
